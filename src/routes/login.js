3c543cc2ef97e146d2c1bd6b018029a6f0a85d16
johannadke <johanna.dke@web.de>
2023-12-12 11:46:19 +0100

    setup

---
+++ src/routes/login.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+
+/* //Codes:
+204: No content
+401: Unauthorized
+500: Internal Server Error
+ */
+let errorSetter = 401;
+let body;
+const tokenWithZeros =
+  "0000000000000000000000000000000000000000000000000000000000000000";
+// Beispiel-Endpunkt für Benutzer (/api/users)
+router.post("/", (req, res) => {
+  switch (errorSetter) {
+    case 204:
+      body = { code: 204 };
+      res.header("Authorization", `Bearer ${tokenWithZeros}`);
+      break;
+    case 404:
+      body = { code: 404, message: "User Not Found" };
+      break;
+    case 401:
+      body = { code: 401, message: "Unauthorized" };
+      break;
+    case 403:
+      body = { code: 403, message: "Forbidden" };
+      break;
+  }
+
+  res.status(body.code);
+
+  if (errorSetter != 204) {
+    const mockData = { error: body };
+    res.json(mockData);
+  }
+
+  res.send();
+});
+
+module.exports = router;
---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const apiVersion = "v1";
+
+// Middleware für JSON-Parser
+app.use(express.json());
+
+// Beispiel-Endpunkt für Benutzer (/api/users)
+app.use(`/api/${apiVersion}/users/login`, require("./routes/login"));
+
+// Beispiel-Endpunkt für Beiträge (/api/imprint)
+app.use(`/api/${apiVersion}/imprint`, require("./routes/imprint"));
+
+// Starte den Server
+app.listen(port, () => {
+  console.log(`Mock-Server läuft auf http://localhost:${port}`);
+});

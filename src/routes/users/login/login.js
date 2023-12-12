2e572331425a5fe9610804f6164875d755887b08
johannadke <johanna.dke@web.de>
2023-12-12 12:47:19 +0100

    feat: jwt in activation

--- src/routes/users/login/login.js
+++ src/routes/users/login/login.js
@@ -7,25 +7,26 @@ const router = express.Router();
 404: User Not Found
 403: Forbidden
  */
-let errorSetter = 401;
-const tokenWithZeros = "0000000000000000000000000000000000000000000000000000000000000000";
-const refreshToken = "REFRESH000000000000000000000000000000000000000000000000000000000";
+let errorSetter = 200;
+const tokenWithZeros =
+  "0000000000000000000000000000000000000000000000000000000000000000";
+const refreshToken =
+  "REFRESH000000000000000000000000000000000000000000000000000000000";
 let mockData;
 
-// Beispiel-Endpunkt für Benutzer (/api/users)
 router.post("/", (req, res) => {
   switch (errorSetter) {
     case 200:
       mockData = { token: tokenWithZeros, refreshToken: refreshToken };
       break;
     case 404:
-      mockData = {error: { code: 404, message: "User Not Found" }};
+      mockData = { error: { code: 404, message: "User Not Found" } };
       break;
     case 401:
-      mockData = {error: { code: 401, message: "Unauthorized" }};
+      mockData = { error: { code: 401, message: "Unauthorized" } };
       break;
     case 403:
-      mockData = {error: { code: 403, message: "Forbidden" }};
+      mockData = { error: { code: 403, message: "Forbidden" } };
       break;
   }
 
--- src/routes/users/register/activateUser.js
+++ src/routes/users/register/activateUser.js
@@ -2,34 +2,32 @@ const express = require("express");
 const router = express.Router();
 
 /* //Codes:
-204: No content
+200: OK
 404: Not Found
 401: Unauthorized
  */
-let errorSetter = 204;
+let errorSetter = 200;
 let body;
+const tokenWithZeros =
+  "0000000000000000000000000000000000000000000000000000000000000000";
+const refreshToken =
+  "REFRESH000000000000000000000000000000000000000000000000000000000";
+let mockData;
 // Beispiel-Endpunkt für Benutzer (/api/users)
 router.post("/", (req, res) => {
   switch (errorSetter) {
-    case 204:
-      body = { code: 204 };
+    case 200:
+      mockData = { token: tokenWithZeros, refreshToken: refreshToken };
       break;
     case 404:
-      body = { code: 404, message: "User Not Found" };
+      mockData = { error: { code: 404, message: "User Not Found" } };
       break;
     case 401:
-      body = { code: 401, message: "Unauthorized" };
+      mockData = { error: { code: 401, message: "Unauthorized" } };
       break;
   }
 
-  res.status(body.code);
-
-  if (errorSetter != 204) {
-    const mockData = { error: body };
-    res.json(mockData);
-  }
-
-  res.send();
+  res.status(errorSetter).json(mockData).send();
 });
 
 module.exports = router;
